0f17f0b74e3e5aa846b806eabc14d2d7462801a4
lindaprlia <126220473+lindaprlia@users.noreply.github.com>
2023-03-10 10:31:44 +0700

    Add files via upload

---
+++ tugasweb3.py
@@ -0,0 +1,20 @@
+user = [
+{
+'nama': 'Arsene Lupin',
+'nik': 1234,
+'jenis_kelamin': 'Male',
+'tanggal_lahir': '1902-03-23'
+},
+{
+'nama': 'Sherlock Holmes',
+'nik': 4321,
+'jenis_kelamin': 'Male',
+'tanggal_lahir': '1876-08-16'
+},
+{
+'nama': 'Miss Marple',
+'nik': 5678,
+'jenis_kelamin': 'Female',
+'tanggal_lahir': '1900-12-15'
+}
+]
